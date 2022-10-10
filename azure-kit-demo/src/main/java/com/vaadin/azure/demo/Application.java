467ce6fe1b2bba7285da5a6aeaad7ebf6f7cd739
Giovanni Lovato <giovanni@lova.to>
2022-10-10 15:03:30 +0200

    chore(demo): Add azure-kit-demo module

---
+++ azure-kit-demo/src/main/java/com/vaadin/azure/demo/Application.java
@@ -0,0 +1,29 @@
+package com.vaadin.azure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "azure-kit-demo")
+@PWA(name = "Azure Kit Demo", shortName = "Azure Kit Demo", offlineResources = {})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+@NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
