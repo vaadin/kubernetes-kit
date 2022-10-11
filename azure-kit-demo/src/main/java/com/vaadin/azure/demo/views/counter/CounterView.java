154277e1d93587dfb689ee54705fc482a884c3d1
Giovanni Lovato <giovanni@lova.to>
2022-10-11 11:52:39 +0200

    Use view field as counter

--- azure-kit-demo/src/main/java/com/vaadin/azure/demo/views/counter/CounterView.java
+++ azure-kit-demo/src/main/java/com/vaadin/azure/demo/views/counter/CounterView.java
@@ -1,14 +1,20 @@
 package com.vaadin.azure.demo.views.counter;
 
-import com.vaadin.azure.demo.services.CounterService;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import com.vaadin.azure.demo.services.HostInfo;
 import com.vaadin.azure.demo.views.MainLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
@@ -18,19 +24,21 @@ import com.vaadin.flow.router.RouteAlias;
 @RouteAlias(value = "", layout = MainLayout.class)
 public class CounterView extends VerticalLayout {
 
-    private final CounterService counterService;
-
     private final H1 counterHeading = new H1();
 
     private final H2 hostnameHeading = new H2();
 
     private final H3 ipAddressHeading = new H3();
 
+    private final Grid<CountEntry> grid = new Grid<>();
+
+    private final List<CountEntry> log = new ArrayList<>();
+
     private final Button button = new Button("Increment");
 
-    public CounterView(CounterService counterService) {
-        this.counterService = counterService;
+    private final AtomicInteger counter = new AtomicInteger();
 
+    public CounterView() {
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
@@ -42,16 +50,52 @@ public class CounterView extends VerticalLayout {
 
         button.addThemeVariants(ButtonVariant.LUMO_PRIMARY,
                 ButtonVariant.LUMO_LARGE);
-        button.addClickListener(event -> updateHeadings());
+        button.addClickListener(event -> count());
 
-        updateHeadings();
+        grid.addColumn(CountEntry::getCount);
+        grid.addColumn(CountEntry::getHostname);
+        grid.addColumn(CountEntry::getIpAddress);
+        grid.setItems(DataProvider.ofCollection(log));
 
-        add(counterHeading, hostnameHeading, ipAddressHeading, button);
+        count();
+
+        add(counterHeading, hostnameHeading, ipAddressHeading, button, grid);
     }
 
-    private void updateHeadings() {
-        counterHeading.setText(counterService.incrementAndGet());
-        hostnameHeading.setText(HostInfo.getHostname());
-        ipAddressHeading.setText(HostInfo.getIpAddress());
+    private void count() {
+        final var entry = new CountEntry(counter.incrementAndGet(),
+                HostInfo.getHostname(), HostInfo.getIpAddress());
+
+        counterHeading.setText(Integer.toString(entry.getCount()));
+        hostnameHeading.setText(entry.getHostname());
+        ipAddressHeading.setText(entry.getIpAddress());
+
+        log.add(0, entry);
+        grid.getDataProvider().refreshAll();
+    }
+
+    static class CountEntry implements Serializable {
+
+        private final int count;
+        private final String hostname;
+        private final String ipAddress;
+
+        public CountEntry(int count, String hostname, String ipAddress) {
+            this.count = count;
+            this.hostname = hostname;
+            this.ipAddress = ipAddress;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public String getHostname() {
+            return hostname;
+        }
+
+        public String getIpAddress() {
+            return ipAddress;
+        }
     }
 }
