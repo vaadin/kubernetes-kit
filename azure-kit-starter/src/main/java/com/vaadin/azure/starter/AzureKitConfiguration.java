55abcac932a8ca6ddf8332273c6c1b7157ee7223
Giovanni Lovato <giovanni@lova.to>
2022-10-13 12:02:19 +0200

    Make repository customizer conditional

--- azure-kit-starter/src/main/java/com/vaadin/azure/starter/AzureKitConfiguration.java
+++ azure-kit-starter/src/main/java/com/vaadin/azure/starter/AzureKitConfiguration.java
@@ -1,6 +1,7 @@
 package com.vaadin.azure.starter;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.session.SaveMode;
 import org.springframework.session.config.SessionRepositoryCustomizer;
@@ -19,7 +20,8 @@ public class AzureKitConfiguration {
      *
      * @return the session-repository customizer
      */
-    @Bean
+    @Bean("vaadinAzureKitRedisSessionRepositoryCustomizer")
+    @ConditionalOnClass(RedisIndexedSessionRepository.class)
     public SessionRepositoryCustomizer<RedisIndexedSessionRepository> redisSessionRepositoryCustomizer() {
         return repository -> {
             repository.setSaveMode(SaveMode.ON_GET_ATTRIBUTE);
