5f2e61ff3b9db336a1bd5fd2efa55c48d3404f91
Marco Collovati <mcollovati@gmail.com>
2022-11-09 14:09:37 +0100

    chore: Remove Spring Session dependencies and code (#40)

--- azure-kit-starter/src/test/java/com/vaadin/azure/starter/AzureKitConfigurationTest.java
+++ azure-kit-starter/src/test/java/com/vaadin/azure/starter/AzureKitConfigurationTest.java
@@ -1,92 +1,16 @@
 package com.vaadin.azure.starter;
 
-import com.hazelcast.config.AttributeConfig;
 import com.hazelcast.config.Config;
-import com.hazelcast.config.IndexConfig;
-import com.hazelcast.config.IndexType;
-import com.hazelcast.config.SerializerConfig;
 import com.hazelcast.core.HazelcastInstance;
 import org.junit.jupiter.api.Test;
-import org.springframework.session.SaveMode;
-import org.springframework.session.data.redis.RedisIndexedSessionRepository;
-import org.springframework.session.hazelcast.Hazelcast4IndexedSessionRepository;
-import org.springframework.session.hazelcast.Hazelcast4PrincipalNameExtractor;
-import org.springframework.session.hazelcast.HazelcastSessionSerializer;
-
-import com.vaadin.azure.starter.AzureKitConfiguration.HazelcastSessionRepositoryConfiguration;
-import com.vaadin.azure.starter.AzureKitConfiguration.RedisSessionRepositoryConfiguration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class AzureKitConfigurationTest {
-
-    @Test
-    public void redisRepositoryAvailable_saveModeConfigured() {
-        var configuration = new RedisSessionRepositoryConfiguration();
-        var customizer = configuration.redisSessionRepositoryCustomizer();
-        var mockRepository = mock(RedisIndexedSessionRepository.class);
-
-        customizer.customize(mockRepository);
-
-        verify(mockRepository).setSaveMode(SaveMode.ON_GET_ATTRIBUTE);
-    }
-
-    @Test
-    public void hazelcastRepositoryAvailable_saveModeConfigured() {
-        var configuration = new HazelcastSessionRepositoryConfiguration(new AzureKitProperties());
-        var customizer = configuration.hazelcastSessionRepositoryCustomizer();
-        var mockRepository = mock(Hazelcast4IndexedSessionRepository.class);
-
-        customizer.customize(mockRepository);
-
-        verify(mockRepository).setSaveMode(SaveMode.ON_GET_ATTRIBUTE);
-    }
-
-    @Test
-    public void hazelcastInstance_principalAttributeConfigured() {
-        var prop = new AzureKitProperties();
-        var configuration = new MockHazelcastConfiguration(prop);
-
-        var hz = configuration.hazelcastInstance();
-        var mapConfig = hz.getConfig().getMapConfig(
-                Hazelcast4IndexedSessionRepository.DEFAULT_SESSION_MAP_NAME);
-        var hasPrincipalAttribute = mapConfig.getAttributeConfigs().stream()
-                .anyMatch(this::matchingPrincipalAttribute);
-
-        assertTrue(hasPrincipalAttribute);
-    }
-
-    @Test
-    public void hazelcastInstance_principalHashIndexConfigured() {
-        var prop = new AzureKitProperties();
-        var configuration = new MockHazelcastConfiguration(prop);
-
-        var hz = configuration.hazelcastInstance();
-        var mapConfig = hz.getConfig().getMapConfig(
-                Hazelcast4IndexedSessionRepository.DEFAULT_SESSION_MAP_NAME);
-        var hasPrincipalHashIndex = mapConfig.getIndexConfigs().stream()
-                .anyMatch(this::matchingPrincipalHashIndex);
-
-        assertTrue(hasPrincipalHashIndex);
-    }
-
-    @Test
-    public void hazelcastInstance_sessionSerializerSet() {
-        var prop = new AzureKitProperties();
-        var configuration = new MockHazelcastConfiguration(prop);
-
-        var hz = configuration.hazelcastInstance();
-        var hasHazelcastSessionSerializer = hz.getConfig()
-                .getSerializationConfig().getSerializerConfigs().stream()
-                .anyMatch(this::matchingHazelcastSessionSerializer);
-
-        assertTrue(hasHazelcastSessionSerializer);
-    }
+class AzureKitConfigurationTest {
 
     @Test
     public void hazelcastInstance_serviceNameSet_kubernetesConfigured() {
@@ -111,27 +35,8 @@ public class AzureKitConfigurationTest {
         assertEquals("foo-service", serviceName);
     }
 
-    private boolean matchingHazelcastSessionSerializer(
-            SerializerConfig config) {
-        return config.getImplementation().getClass()
-                .equals(HazelcastSessionSerializer.class);
-    }
-
-    private boolean matchingPrincipalHashIndex(IndexConfig config) {
-        return config.getType().equals(IndexType.HASH)
-                && config.getAttributes().contains(
-                        Hazelcast4IndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE);
-    }
-
-    private boolean matchingPrincipalAttribute(AttributeConfig config) {
-        return config.getName().equals(
-                Hazelcast4IndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
-                && config.getExtractorClassName().equals(
-                        Hazelcast4PrincipalNameExtractor.class.getName());
-    }
-
     private final class MockHazelcastConfiguration
-            extends AzureKitConfiguration.HazelcastSessionRepositoryConfiguration {
+            extends AzureKitConfiguration.HazelcastConfiguration {
 
         public MockHazelcastConfiguration(AzureKitProperties properties) {
             super(properties);
