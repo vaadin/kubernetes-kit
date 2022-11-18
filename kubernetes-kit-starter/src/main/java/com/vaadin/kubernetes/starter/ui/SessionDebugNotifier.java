7f4e457db0afb10bd067e0ef6b8fa072bb740955
Marco Collovati <mcollovati@gmail.com>
2022-11-18 13:40:41 +0100

    fix: run debug tool after Vaadin request is processed (#55)
    * fix: run debug tool after Vaadin request is processed
    
    Debug Tool is now executed by an HTTP Filter at the end of the request.
    The Filter gets an executable command set by the RequestHandler
    as request attribute, only for UIDL requests.
    If PUSH is enabled, a notification will visible on the browser
    in case of failures.
    
    Fixes #44
    Fixes #46
    
    * chore: renamed notifier component and removed commented code

---
+++ kubernetes-kit-starter/src/main/java/com/vaadin/kubernetes/starter/ui/SessionDebugNotifier.java
@@ -0,0 +1,32 @@
+package com.vaadin.kubernetes.starter.ui;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.kubernetes.starter.sessiontracker.serialization.SerializationDebugRequestHandler;
+
+import elemental.json.Json;
+import elemental.json.JsonObject;
+
+@Tag("vaadin-session-debug-notifier")
+@JsModule("./components/session-debug-notifier.ts")
+public class SessionDebugNotifier extends Component {
+
+    public void publishResults(SerializationDebugRequestHandler.Result result) {
+        // Add serialization result to show notifications on browser
+        JsonObject propValue = Json.createObject();
+        // just to make sure property is always detected as changed since when
+        // closing notification value is nullified only on client side
+        propValue.put("timestamp", System.nanoTime());
+        if (result.getOutcomes()
+                .contains(SerializationDebugRequestHandler.Outcome.SUCCESS)) {
+            propValue.put("success", true);
+        } else {
+            propValue.put("success", false);
+            propValue.put("message", result.getOutcomes().toString());
+        }
+        propValue.put("duration", result.getDuration());
+        getElement().setPropertyJson("outcome", propValue);
+    }
+
+}
