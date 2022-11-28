09b9488ea73c2dcaf0a353991621b584774e5b87
Matthew Wilson <matthew_w@vaadin.com>
2022-11-28 12:09:27 +0200

    Apply suggestions from PR

--- kubernetes-kit-starter/src/test/java/com/vaadin/kubernetes/starter/sessiontracker/push/NotifyingPushConnectionTest.java
+++ kubernetes-kit-starter/src/test/java/com/vaadin/kubernetes/starter/sessiontracker/push/NotifyingPushConnectionTest.java
@@ -3,10 +3,14 @@ package com.vaadin.kubernetes.starter.sessiontracker.push;
 import org.atmosphere.cpr.AtmosphereResource;
 import org.atmosphere.cpr.Broadcaster;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.di.Lookup;
 import com.vaadin.flow.server.communication.PushConnection;
-import com.vaadin.kubernetes.starter.util.MockUI;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
@@ -14,14 +18,17 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class NotifyingPushConnectionTest {
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private UI ui;
+
     @Test
     void sendMessage_listenersAreNotified() {
-        MockUI ui = new MockUI();
         PushSendListener listener = mock(PushSendListener.class);
         Lookup lookup = Lookup.of(listener, PushSendListener.class);
-        ui.getSession().getService().getContext()
-                .setAttribute(Lookup.class, lookup);
+        when(ui.getSession().getService().getContext()
+                .getAttribute(Lookup.class)).thenReturn(lookup);
 
         AtmosphereResource resource = mock(AtmosphereResource.class);
         Broadcaster broadcaster = mock(Broadcaster.class);
@@ -37,7 +44,7 @@ public class NotifyingPushConnectionTest {
     @Test
     void factory_apply_returnsNotifyingPushConnection() {
         NotifyingPushConnection.Factory factory = new NotifyingPushConnection.Factory();
-        PushConnection connection = factory.apply(new MockUI());
+        PushConnection connection = factory.apply(ui);
 
         assertTrue(connection instanceof NotifyingPushConnection);
     }
