c903141b221a74920fce8c8fa17eeae54146fc81
Giovanni Lovato <giovanni@vaadin.com>
2023-09-22 11:05:09 +0200

    chore: Bump Flow and components to 24.2 (#88)

--- kubernetes-kit-demo/src/main/java/com/vaadin/kubernetes/demo/generator/DataGenerator.java
+++ kubernetes-kit-demo/src/main/java/com/vaadin/kubernetes/demo/generator/DataGenerator.java
@@ -1,31 +1,31 @@
 package com.vaadin.kubernetes.demo.generator;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.vaadin.artur.exampledata.DataType;
-import org.vaadin.artur.exampledata.ExampleDataGenerator;
 
+import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.kubernetes.demo.entity.Company;
 import com.vaadin.kubernetes.demo.entity.Contact;
 import com.vaadin.kubernetes.demo.entity.Status;
 import com.vaadin.kubernetes.demo.repository.CompanyRepository;
 import com.vaadin.kubernetes.demo.repository.ContactRepository;
 import com.vaadin.kubernetes.demo.repository.StatusRepository;
-import com.vaadin.flow.spring.annotation.SpringComponent;
 
 @SpringComponent
 public class DataGenerator {
 
     @Bean
-    public CommandLineRunner loadData(ContactRepository contactRepository, CompanyRepository companyRepository,
+    CommandLineRunner loadData(ContactRepository contactRepository, CompanyRepository companyRepository,
             StatusRepository statusRepository) {
 
         return args -> {
@@ -37,27 +37,36 @@ public class DataGenerator {
             int seed = 123;
 
             logger.info("Generating demo data");
-            ExampleDataGenerator<Company> companyGenerator = new ExampleDataGenerator<>(Company.class,
-                    LocalDateTime.now());
-            companyGenerator.setData(Company::setName, DataType.COMPANY_NAME);
-            List<Company> companies = companyRepository.saveAll(companyGenerator.create(5, seed));
+            Faker faker = new Faker();
+            Supplier<Company> companyGenerator = () -> {
+                Company company = new Company();
+                company.setName(faker.company().name());
+                return company;
+            };
+            List<Company> companies = companyRepository.saveAll(Stream.generate(companyGenerator).limit(5).toList());
 
             List<Status> statuses = statusRepository
                     .saveAll(Stream.of("Imported lead", "Not contacted", "Contacted", "Customer", "Closed (lost)")
                             .map(Status::new).collect(Collectors.toList()));
 
             logger.info("... generating 50 Contact entities...");
-            ExampleDataGenerator<Contact> contactGenerator = new ExampleDataGenerator<>(Contact.class,
-                    LocalDateTime.now());
-            contactGenerator.setData(Contact::setFirstName, DataType.FIRST_NAME);
-            contactGenerator.setData(Contact::setLastName, DataType.LAST_NAME);
-            contactGenerator.setData(Contact::setEmail, DataType.EMAIL);
+            Supplier<Contact> contactGenerator = () -> {
+                Contact contact = new Contact();
+                Name name = faker.name();
+                contact.setFirstName(name.firstName());
+                contact.setLastName(name.lastName());
+                contact.setEmail(faker.internet().emailAddress());
+                return contact;
+            };
 
             Random r = new Random(seed);
-            List<Contact> contacts = contactGenerator.create(50, seed).stream().peek(contact -> {
-                contact.setCompany(companies.get(r.nextInt(companies.size())));
-                contact.setStatus(statuses.get(r.nextInt(statuses.size())));
-            }).collect(Collectors.toList());
+            List<Contact> contacts = Stream.generate(contactGenerator)
+                    .peek(contact -> {
+                        int companyIndex = r.nextInt(companies.size());
+                        contact.setCompany(companies.get(companyIndex));
+                        int contactIndex = r.nextInt(statuses.size());
+                        contact.setStatus(statuses.get(contactIndex));
+                    }).limit(50).toList();
 
             contactRepository.saveAll(contacts);
 
