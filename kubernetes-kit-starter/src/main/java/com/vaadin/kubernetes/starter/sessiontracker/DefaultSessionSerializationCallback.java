6d2e44178e1712103cb0f8d8cdcba6ccea371252
tamasmak <maktamas@gmail.com>
2024-03-11 15:47:15 +0100

    Remove DefaultSessionSerializationCallback class

--- kubernetes-kit-starter/src/main/java/com/vaadin/kubernetes/starter/sessiontracker/DefaultSessionSerializationCallback.java
+++
@@ -1,9 +0,0 @@
-package com.vaadin.kubernetes.starter.sessiontracker;
-
-/**
- * Default error callbacks that are called after a serialization or
- * deserialization error happens.
- */
-public class DefaultSessionSerializationCallback
-        implements SessionSerializationCallback {
-}
