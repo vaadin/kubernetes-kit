186df8bc09ceebd0f2771d78aa3dc737e910a2c0
Tim Kluge <Tim.Kluge@saxms.de>
2024-11-29 09:27:54 +0100

    Adds pluggable SerializationStreamFactory

---
+++ kubernetes-kit-starter/src/main/java/com/vaadin/kubernetes/starter/sessiontracker/serialization/SerializationStreamFactory.java
@@ -0,0 +1,26 @@
+/*-
+ * Copyright (C) 2024 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
+package com.vaadin.kubernetes.starter.sessiontracker.serialization;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.function.Predicate;
+
+/**
+ * Factory that is used to create new input / output streams for session (de-)serialization.
+ *
+ * @see TransientInjectableObjectStreamFactory
+ **/
+public interface SerializationStreamFactory {
+    SerializationOutputStream createOutputStream(OutputStream baseOutputStream, TransientHandler transientHandler, Predicate<Class<?>> injectableFilter) throws IOException;
+
+    SerializationInputStream createInputStream(InputStream in, TransientHandler transientHandler) throws IOException;
+}
